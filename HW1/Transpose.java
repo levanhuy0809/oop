65bbf8305f0f0b6b67718a2abf4f53a60805c4a4
levanhuy08092006 <levanhuy080906@gmail.com>
2025-09-30 16:40:40 +0700

    5 bai tap tu chon

---
+++ HW1/Transpose.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Transpose {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
